95d282cb76d33be53edb181cbca10e2da87d39ea
Rémy Lavainne <remy.lavainne@ensimag.grenoble-inp.fr>
2020-01-28 08:27:54 +0100

    Suppression TP multimedia

--- wmlab/include/masking.h
+++
@@ -1,9 +0,0 @@
-#include <it/mat.h>
-
-#ifndef _MASKING_H_
-#define _MASKING_H_
-
-mat    mat_sobel( mat x );
-mat    mat_mask_custom( mat x );
-
-#endif
--- wmlab/include/modulation.h
+++
@@ -1,8 +0,0 @@
-#include <it/mat.h>
-
-#ifndef _MODULATION_H_
-#define _MODULATION_H_
-
-vec encode_ss( vec x, bvec m, mat u, double wcr ); 
-
-#endif 
